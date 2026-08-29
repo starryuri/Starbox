29b557d77c75b309fad99409e689b291777cc941
starryuri <starryuri@users.noreply.github.com>
2026-08-30 04:22:41 +0800

    chore(release): bump version to 1.2.4

--- cmd/setup/main.go
+++ cmd/setup/main.go
@@ -37,7 +37,7 @@ var uninsExe []byte
 const (
 	uninstallKey   = `HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\STARBOX`
 	appKey         = `HKCU\Software\STARBOX`
-	appVersion     = "1.2.3"
+	appVersion     = "1.2.4"
 	createNoWindow = 0x08000000
 )
 
